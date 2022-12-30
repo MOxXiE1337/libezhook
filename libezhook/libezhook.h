e0e6e1f0fcda92ec3c3c2d5dbb1a293769036600
摩西 <88440719+MOxXiE1337@users.noreply.github.com>
2022-12-30 16:47:16 +0800

    recode some functions

--- libezhook/libezhook.h
+++ libezhook/libezhook.h
@@ -1,36 +1,16 @@
 #pragma once
-#include <Windows.h>
 
-typedef struct _ezhook
+extern "C"
 {
-	void* target;
-	void* detour;
-	void* ret_addr;
-	void* trampline;
-	unsigned char original_bytes[5];
-	unsigned char new_bytes[5];
-	void* ebx;
-	void* esi;
-	void* edi;
-	struct _ezhook* prev;
-	struct _ezhook* next;
-} ezhook;
+	// hook a function, return its trampline
+	void* __stdcall hook(void* target, void* detour);
 
-typedef struct _ezhook_entry
-{
-	void* target; // key
-	ezhook* hooks;
-	struct _ezhook_entry* next;
-} ezhook_entry;
+	// hook a api function, return its trampline
+	void* __stdcall hook_api(const char* module_name, const char* proc_name, void* detour);
 
-// hook a function, return its trampline
-__declspec(dllexport) void* __stdcall hook(void* target, void* detour);
+	// hook a virtual function, return its trampline
+	void* __stdcall hook_virtual(void* class_pointer, unsigned int index, void* detour);
 
-// hook a api function, return its trampline
-__declspec(dllexport) void* __stdcall hook_api(const char* module_name, const char* proc_name, void* detour);
-
-// hook a virtual function, return its trampline
-__declspec(dllexport) void* __stdcall hook_virtual(void* class_pointer, unsigned int index, void* detour);
-
-// unhook a function
-__declspec(dllexport) void __stdcall unhook(void* target, void* detour);
+	// unhook a function
+	void __stdcall unhook(void* target, void* detour);
+}
